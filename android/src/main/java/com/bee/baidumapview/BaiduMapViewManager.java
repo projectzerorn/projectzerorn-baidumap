8a56fee41d78505198956290ba9f494e43817c79
jackzhou <fiattt@msn.com>
2016-10-20 17:34:33 +0800

    fix crash

--- android/src/main/java/com/bee/baidumapview/BaiduMapViewManager.java
+++ android/src/main/java/com/bee/baidumapview/BaiduMapViewManager.java
@@ -15,7 +15,6 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-import com.facebook.react.uimanager.LayoutShadowNode;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
@@ -42,16 +41,6 @@ public class BaiduMapViewManager extends SimpleViewManager<MapView> implements B
     protected MapView createViewInstance(ThemedReactContext reactContext) {
         this.reactContext = reactContext;
         return getMap();
-    }
-
-    @Override
-    public LayoutShadowNode createShadowNodeInstance() {
-        return new BaiduMapShadowNode();
-    }
-
-    @Override
-    public Class getShadowNodeClass() {
-        return BaiduMapShadowNode.class;
     }
 
     public BaiduMapViewManager(Activity activity) {
